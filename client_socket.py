01d0b4a6cd476fd98575750f96c9b45ddb94c2c0
Anthony Orlandi <Agorlandi@comcast.net>
2026-04-15 20:52:21 -0700

    client and server scripts work

--- client_socket.py
+++ client_socket.py
@@ -3,20 +3,21 @@ import socket
 
 #Define Host and Port
 HOST = 'scanme.nmap.org'
-PORT = 65432
+PORT = 80
 
 #Create TCP/IP Socket and Start Client
 def start_client():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5.0)
             s.connect((HOST, PORT))
-            message = "Hello there!"
+            message = "GET /HTTP/1.1\r\nHost: scanme.nmap.org\r\n\r\n"
             s.sendall(message.encode())
             data = s.recv(1024)
             print(f"Received from server: {data.decode()}")
     #Error handling for connection issues
     except Exception as e: 
-        print(f"An error occured: {e}")
+        print(f"An error occurred: {e}")
         
 
 if __name__ == "__main__":
--- server_socket.py
+++ server_socket.py
@@ -1,7 +1,8 @@
 #Import Socket Library
 import socket
+import threading
 #Define Host and Port
-HOST = 'scanme.nmap.org'
+HOST = '127.0.0.1'
 PORT = 65432
 #Create TCP/IP Socket and Start Server
 def start_server():
@@ -11,17 +12,22 @@ def start_server():
             s.bind((HOST, PORT))
             s.listen()
             print(f"Server is listening on {HOST}:{PORT}")
-            conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True: 
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"Received data: {data.decode()}")
-                conn.sendall(data)
+            while True:
+                conn, addr = s.accept()
+                thread = threading.Thread(target=handle_client, args=(conn, addr))
+                thread.start()
     except Exception as e: 
-        print(f"An error occured: {e}")
+        print(f"An error occurred: {e}")
+
+def handle_client(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True: 
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Received data: {data.decode()}")
+            conn.sendall(data)
 
 if __name__ == "__main__":
     start_server()

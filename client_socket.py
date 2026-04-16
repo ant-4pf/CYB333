4a9a498f0cd126f4ea35264cb6a1e142c7e8254b
Anthony Orlandi <Agorlandi@comcast.net>
2026-04-15 21:04:58 -0700

    client modified

--- client_socket.py
+++ client_socket.py
@@ -11,7 +11,7 @@ def start_client():
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(5.0)
             s.connect((HOST, PORT))
-            message = "GET /HTTP/1.1\r\nHost: scanme.nmap.org\r\n\r\n"
+            message = "GET / HTTP/1.1\r\nHost: scanme.nmap.org\r\n\r\n" 
             s.sendall(message.encode())
             data = s.recv(1024)
             print(f"Received from server: {data.decode()}")

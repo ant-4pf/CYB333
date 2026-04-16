da9d7ea99e5535e6ca8db6a52f43d56f2a73b0e5
Anthony Orlandi <Agorlandi@comcast.net>
2026-04-15 20:18:22 -0700

    updated midterm

--- client_socket.py
+++ client_socket.py
@@ -1,7 +1,11 @@
+#Import socket library
 import socket
+
+#Define Host and Port
 HOST = '127.0.0.1'
 PORT = 65432
 
+#Create TCP/IP Socket and Start Client
 def start_client():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -10,6 +14,7 @@ def start_client():
             s.sendall(message.encode())
             data = s.recv(1024)
             print(f"Received from server: {data.decode()}")
+    #Error handling for connection issues
     except Exception as e: 
         print(f"An error occured: {e}")
         
---
+++ server_socket.py
@@ -0,0 +1,31 @@
+#Import Socket Library
+import socket
+#Define Host and Port
+HOST = '127.0.0.1'
+PORT = 65432
+#Create TCP/IP Socket and Start Server
+def start_server():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            #Bind socket and port, then listen for incoming connections
+            s.bind((HOST, PORT))
+            s.listen()
+            print(f"Server is listening on {HOST}:{PORT}")
+            conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True: 
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"Received data: {data.decode()}")
+                conn.sendall(data)
+    except Exception as e: 
+        print(f"An error occured: {e}")
+
+if __name__ == "__main__":
+    start_server()
+            
+    
+
+

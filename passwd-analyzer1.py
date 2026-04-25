dd17f144b4d463d2cb31cacf740f46cb140158f3
Anthony Orlandi <Agorlandi@comcast.net>
2026-04-25 15:05:16 -0700

    Password Analyzer

--- passwd-analyzer1.py
+++ passwd-analyzer1.py
@@ -1,34 +1,49 @@
 print("Password Strength Analyzer")
 
+import getpass
 
-password = input("Enter your password: ")
+password = getpass.getpass("Enter your password: ")
 length = len(password)
-SC = "!@#$%^&*()-_=+[]}{/?<>|:\;\`~"
+SC = "!@#$%^&*()|-_=+[]}{/?<>:"
 strength_score = 0
+max_length = 25
 
-
-def calculate_strength(password):
-    global strength_score
-    if any(char.islower() for char in password):
-        strength_score += 1
-    if any(char.isupper() for char in password):
-        strength_score += 1
-    if any(char.isdigit() for char in password):
-        strength_score += 2
-    if any(char in SC for char in password):
-        strength_score += 2
-    if length < 8:
-        print("Password should be at least 8 characters long.")
-    elif length >= 8 and length < 12:
-        strength_score += 1
-        print("Password is good, but could be stronger.")
-    elif length >= 12 and length >= 16:
-        strength_score += 3
-        print("Password is strong!")
-    else:
-        strength_score += 2
-        print("Password is VERY strong!")
+def calculate_strength(password, length):
+    score = 0
+    feedback = ""
+    
+    # Check character types
+    has_lower = any(char.islower() for char in password)
+    has_upper = any(char.isupper() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(char in SC for char in password)
+    
+    if has_lower:
+        score += 1
+    if has_upper:
+        score += 1
+    if has_digit:
+        score += 2
+    if has_special:
+        score += 2
+    
+    # Length-based scoring and feedback
+    if length > max_length:
+        feedback = "Password is too long. Consider using a shorter password."
+    elif length < 8:
+        feedback = "Password should be at least 8 characters long."
+    elif 8 <= length < 12:
+        score += 1
+        feedback = "Password is good, but could be stronger."
+    elif 12 <= length < 16:
+        score += 2
+        feedback = "Password is strong!"
+    else:  # length >= 16
+        score += 3
+        feedback = "Password is VERY strong!"
+    return score, feedback
 
 print("Analyzing password strength. . .")
-calculate_strength(password)
+strength_score, feedback = calculate_strength(password, length)
+print(feedback)
 print(f"Password Strength Score: {strength_score}") 
